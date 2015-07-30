515e6c443c25d3885380c6d12ffd985d3815454a
Tiago Mendes-Costa <tiago@mendes-costa.net>
2015-07-30 20:50:00 +0200

    Refactor the Enigma specs.

--- spec/enigma/enigma_spec.rb
+++ spec/enigma/enigma_spec.rb
@@ -1,65 +1,66 @@
 require 'spec_helper'
 
 describe Enigma::Enigma do
-  let(:enigma) do
-    Enigma::Enigma.new(
+  let(:enigma) { Enigma::Enigma.new }
+
+  it 'creates a new Enigma machine with rotors and reflector' do
+    enigma = Enigma::Enigma.new(
       rotors: [Enigma::RotorI.new, Enigma::RotorII.new, Enigma::RotorIII.new],
       reflector: Enigma::ReflectorB.new
     )
+    expect(enigma.is_a?(Enigma::Enigma)).to eq true
+    expect(enigma.rotors[0].is_a?(Enigma::RotorI)).to eq true
+    expect(enigma.rotors[1].is_a?(Enigma::RotorII)).to eq true
+    expect(enigma.rotors[2].is_a?(Enigma::RotorIII)).to eq true
+    expect(enigma.reflector.is_a?(Enigma::ReflectorB)).to eq true
   end
 
-  it 'resets the rotors' do
-    enigma.reset(rotations: 'BCD', rings: 'EFG')
-    expect(enigma.rotors[0].rotation).to eq 'B'
-    expect(enigma.rotors[1].rotation).to eq 'C'
-    expect(enigma.rotors[2].rotation).to eq 'D'
-    expect(enigma.rotors[0].ring).to eq 'E'
-    expect(enigma.rotors[1].ring).to eq 'F'
-    expect(enigma.rotors[2].ring).to eq 'G'
-  end
-
-  it 'resets the rotors with rings numerals' do
-    enigma.reset(rings: [5, 6, 7])
-    expect(enigma.rotors[0].ring).to eq 'E'
-    expect(enigma.rotors[1].ring).to eq 'F'
-    expect(enigma.rotors[2].ring).to eq 'G'
-  end
-
-  it 'resets the plugboard' do
-    enigma.reset(plugboard: %w(AV BS CG DL FU HZ IN KM OW RX))
-    expect(enigma.plugboard('A')).to eq 'V'
-    expect(enigma.plugboard('V')).to eq 'A'
-    expect(enigma.plugboard('E')).to eq 'E'
-  end
-
-  context 'rotations and rings set to AAA' do
+  context 'sample Enigma' do
     before do
-      enigma.reset(rotations: 'AAA', rings: 'AAA')
+      enigma.reset(
+        rotors: [Enigma::RotorI.new, Enigma::RotorII.new, Enigma::RotorIII.new],
+        reflector: Enigma::ReflectorB.new
+      )
     end
 
-    it 'encodes a message' do
+    it 'resets the rotors' do
+      enigma.reset(rotations: 'BCD', rings: 'EFG')
+      expect(enigma.rotors[0].rotation).to eq 'B'
+      expect(enigma.rotors[1].rotation).to eq 'C'
+      expect(enigma.rotors[2].rotation).to eq 'D'
+      expect(enigma.rotors[0].ring).to eq 'E'
+      expect(enigma.rotors[1].ring).to eq 'F'
+      expect(enigma.rotors[2].ring).to eq 'G'
+    end
+
+    it 'resets the rotors with rings numerals' do
+      enigma.reset(rings: [5, 6, 7])
+      expect(enigma.rotors[0].ring).to eq 'E'
+      expect(enigma.rotors[1].ring).to eq 'F'
+      expect(enigma.rotors[2].ring).to eq 'G'
+    end
+
+    it 'resets the plugboard' do
+      enigma.reset(plugboard: %w(AV BS CG DL FU HZ IN KM OW RX))
+      expect(enigma.plugboard('A')).to eq 'V'
+      expect(enigma.plugboard('V')).to eq 'A'
+      expect(enigma.plugboard('E')).to eq 'E'
+    end
+
+    it 'encodes a message with rotations and rings set to AAA' do
+      enigma.reset(rotations: 'AAA', rings: 'AAA')
       encoded_message = enigma.encode('AAAAA')
       expect(encoded_message).to eq 'BDZGO'
     end
-  end
 
-  context 'rings set to BBB' do
-    before do
+    it 'encodes a message with rings set to BBB' do
       enigma.reset(rotations: 'AAA', rings: 'BBB')
-    end
-
-    it 'encodes a message' do
       encoded_message = enigma.encode('AAAAA')
       expect(encoded_message).to eq 'EWTYX'
     end
-  end
 
-  context 'rings set to 02 02 02' do
-    before do
+    it 'encodes a message with rings set to 02 02 02' do
       enigma.reset(rotations: 'AAA', rings: [2, 2, 2])
-    end
-
-    it 'encodes a message' do
       encoded_message = enigma.encode('AAAAA')
       expect(encoded_message).to eq 'EWTYX'
     end
@@ -67,22 +68,23 @@ describe Enigma::Enigma do
 
   # Settings and result from
   # http://www.enigmaworldcodegroup.com/#!create-an-enigma-message/c1lth
-  context 'plugboard set' do
+  context 'WCG Enigma' do
     before do
       enigma.reset(
         rotors: [Enigma::RotorIII.new, Enigma::RotorV.new, Enigma::RotorII.new],
+        reflector: Enigma::ReflectorB.new,
         rotations: 'TMK',
         rings: [8, 6, 23],
         plugboard: %w(BJ CY DX EZ FH GO LN MW QV TU)
       )
     end
 
-    it 'encodes a message' do
+    it 'encodes a message with the plugboard' do
       encoded_message = enigma.encode('MYDOG')
       expect(encoded_message).to eq 'CHESY'
     end
 
-    it 'decodes a message' do
+    it 'decodes a message with the plugboard' do
       encoded_message = enigma.decode('CHESY')
       expect(encoded_message).to eq 'MYDOG'
     end
@@ -91,10 +93,11 @@ describe Enigma::Enigma do
   # Settings and result from
   # http://operationturing.tumblr.com/post/120671980865
   # http://operationturing.tumblr.com/post/120819236115
-  context 'operation turing - 5 june 1940' do
+  context 'Operation Turing (5 june 1940)' do
     before do
       enigma.reset(
         rotors: [Enigma::RotorII.new, Enigma::RotorIV.new, Enigma::RotorIII.new],
+        reflector: Enigma::ReflectorB.new,
         rings: [16, 3, 10],
         plugboard: %w(AJ BH CI FV KU LW MZ NX OY PS)
       )
